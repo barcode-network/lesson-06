b320d5f26c40d2171355a66da9d00da56f7213af
Shannon Clarke <shannonajclarke@gmail.com>
2019-02-02 12:17:25 -0400

    Add function to trigger contract function

--- front/src/App.js
+++ front/src/App.js
@@ -12,17 +12,41 @@ import data from './pets.json'
 
 class App extends Component {
 
+  constructor(props){
+    super(props)
+
+    this.state = {
+      account:null,
+      web3:null,
+      contract:null,
+    }
+  }
+
   componentDidMount = async () => {
     const web3 = new Web3(Web3.givenProvider)
 
     const accounts = await web3.eth.getAccounts()
 
-    //Instantiate the polyToken smart contract
-    //***TODO: Grab deployed contract address from commandline */
+    //Instantiate the smart contract
     const contractInstance = new web3.eth.Contract(Customers.abi, '0x36d32F8a3582De966671dC61dc729E47364F61b4')
-    // console.log(accounts, tutorialInstance)
-    //Get account's POLY balance
+
     console.log(accounts, contractInstance)
+
+    this.setState({account:accounts[0],contract:contractInstance})
+
+  }
+
+  handleAdopt = (data)=>{
+    // e.preventDefault()
+    console.log(data)
+    const {contract, account} = this.state
+
+    contract.methods.claim(data.id).send({ from: account, gas: 1000000 })
+      .then((result) => {
+
+        console.log(result)
+
+      })
 
   }
 
@@ -34,20 +58,20 @@ class App extends Component {
         <td>{value.age}</td>
         <td>{value.location}</td>
         <td><img height="100" src={value.picture}/></td>
-        <td><button>Claim</button></td>
+        <td><button onClick={()=>this.handleAdopt(value)}>Claim</button></td>
       </tr>
     );
 
     return (
       <div className="App">
         <table>
-          <tr>
+          <thead>
             <th>Name</th>
             <th>Age</th>
             <th>Location</th>
             <th>Photo</th>
             <th>Action</th>
-          </tr>
+          </thead>
           {tableItems}
         </table>
       </div>
